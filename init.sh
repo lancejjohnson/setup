7db5ff0fd8ff2d3df431e014ce1913056a74144a
Lance Johnson <lancejjohnson@gmail.com>
2018-01-20 11:31:28 -0500

    Update init.sh
    Cannot use git on a fresh macOS installation before installing Xcode.

--- init.sh
+++ init.sh
@@ -9,8 +9,9 @@ if test ! $(which git); then
   echo "git is not installed; cannot clone repositories; exiting!"
   exit 1
 else
-  echo "Cloning git repositories for initialization ..."
-  git clone https://github.com/lancejjohnson/setup.git $HOME/.setup
+  echo "Installing setup scripts ..."
+  curl -L https://api.github.com/repos/lancejjohnson/setup/tarball | tar xz --strip=1 -C $HOME/.setup
+  # git clone https://github.com/lancejjohnson/setup.git $HOME/.setup
   cd $HOME/.setup
   source ./setup.sh
 fi
