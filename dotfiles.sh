e0d51ba7445b40bc4e44d99711806709fc4dd13b
Lance Johnson <lancejjohnson@gmail.com>
2018-01-19 21:33:18 -0500

    Clone and symlink dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-# Download and sync files from dotfiles
-if test $(which git); then
-  mkdir ~/.dotfiles
+set -e
 
-  echo "Cloning git repositories for initialization"
-  git clone git@github.com:lancejjohnson/dotfiles.git ~/.dotfiles
-else
-  echo "git is not installed; getting dotfiles via curl"
-  mkdir ~/.tmp && cd ~/.tmp
-
-  curl -L https://api.github.com/repos/lancejjohnson/dotfiles/tarball \
-    | tar -xzv --exclude={README.markdown}
+if test ! $(which git); then
+  echo "git is not installed; cannot clone dotfiles"
+  exit 1
+  # curl -L https://api.github.com/repos/lancejjohnson/dotfiles/tarball \
+  #   | tar -xzv --exclude={README.markdown}
 fi
+
+# Clone dotfiles
+echo "Cloning git repositories for initialization ..."
+git clone https://github.com/lancejjohnson/dotfiles.git ~/.dotfiles
+
+# Check for rcm
+if test ! $(which rcup); then
+  echo "rcm is not installed ..."
+  brew tap thoughtbot/formulae
+  brew install rcm
+  brew cleanup
+fi
+
+# Symlink all files in ~/.dotfiles that are not preceded by .
+rcup -f 2> $HOME/.setup/last_run_errors.txt
