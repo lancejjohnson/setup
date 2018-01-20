1c07896942d6debe6b253128bc3725d898cd176a
Lance Johnson <lancejjohnson@gmail.com>
2018-01-20 14:07:27 -0500

    Update osconfig.sh
    Name computer

--- osconfig.sh
+++ osconfig.sh
@@ -8,6 +8,12 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Set computer name (as done via System Preferences → Sharing)
+sudo scutil --set ComputerName "theshire"
+sudo scutil --set HostName "theshire"
+sudo scutil --set LocalHostName "theshire"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "theshire"
+
 # Disable Notification Center and remove the menu bar icon
 launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
