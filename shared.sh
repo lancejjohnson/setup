e66640b1c2b0527b6739790cf6c2751cca5c5e9b
Lance Johnson <lancejjohnson@gmail.com>
2021-01-10 17:56:35 -0500

    Update homebrew installation command

--- shared.sh
+++ shared.sh
@@ -32,7 +32,7 @@ function ensure_homebrew {
 
   if test ! $(which brew); then
     echo "Installing homebrew ..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
 
   if brew list | grep -Fq brew-cask; then
